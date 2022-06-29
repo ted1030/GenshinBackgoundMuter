c67055431e41bf8aeea18d062e78ad9bf0650077
ted1030 <htl8610@gmail.com>
2022-06-30 02:30:25 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace BGMuter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.啟用ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.開機時啟動ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.離開ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(344, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 19);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "BGMuter";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.啟用ToolStripMenuItem,
+            this.開機時啟動ToolStripMenuItem,
+            this.離開ToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(154, 76);
+			// 
+			// 啟用ToolStripMenuItem
+			// 
+			this.啟用ToolStripMenuItem.CheckOnClick = true;
+			this.啟用ToolStripMenuItem.Name = "啟用ToolStripMenuItem";
+			this.啟用ToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
+			this.啟用ToolStripMenuItem.Text = "啟用";
+			this.啟用ToolStripMenuItem.CheckedChanged += new System.EventHandler(this.啟用ToolStripMenuItem_CheckedChanged);
+			// 
+			// 開機時啟動ToolStripMenuItem
+			// 
+			this.開機時啟動ToolStripMenuItem.CheckOnClick = true;
+			this.開機時啟動ToolStripMenuItem.Name = "開機時啟動ToolStripMenuItem";
+			this.開機時啟動ToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
+			this.開機時啟動ToolStripMenuItem.Text = "開機時啟動";
+			this.開機時啟動ToolStripMenuItem.CheckedChanged += new System.EventHandler(this.開機時啟動ToolStripMenuItem_CheckedChanged);
+			// 
+			// 離開ToolStripMenuItem
+			// 
+			this.離開ToolStripMenuItem.Name = "離開ToolStripMenuItem";
+			this.離開ToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
+			this.離開ToolStripMenuItem.Text = "離開";
+			this.離開ToolStripMenuItem.Click += new System.EventHandler(this.離開ToolStripMenuItem_Click);
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(12, 9);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(254, 246);
+			this.checkedListBox1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(471, 272);
+			this.Controls.Add(this.checkedListBox1);
+			this.Controls.Add(this.label1);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private Label label1;
+		private NotifyIcon notifyIcon1;
+		private ContextMenuStrip contextMenuStrip1;
+		private ToolStripMenuItem 開機時啟動ToolStripMenuItem;
+		private ToolStripMenuItem 離開ToolStripMenuItem;
+		private ToolStripMenuItem 啟用ToolStripMenuItem;
+		private CheckedListBox checkedListBox1;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+//using NAudio.CoreAudioApi;
+//using NAudio.CoreAudioApi.Interfaces;
+
+namespace BGMuter
+{
+	public partial class Form1 : Form
+	{
+		//class NotificationClient : IMMNotificationClient
+		//{
+		//	Form1 form;
+		//	public NotificationClient(Form1 form1)
+		//	{
+		//		form = form1;
+		//	}
+		//	void IMMNotificationClient.OnDeviceStateChanged(string deviceId, DeviceState newState) { }
+		//	void IMMNotificationClient.OnDeviceAdded(string pwstrDeviceId) { }
+		//	void IMMNotificationClient.OnDeviceRemoved(string deviceId) { }
+		//	void IMMNotificationClient.OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key) { }
+		//	void IMMNotificationClient.OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId)
+		//	{
+		//		if (defaultDeviceId != null)
+		//			form.SetMute();
+		//	}
+		//}
+
+		class Observer : IObserver<DeviceChangedArgs>
+		{
+			Form1 form;
+			public Observer(Form1 form1)
+			{
+				form = form1;
+			}
+			public void OnCompleted() { }
+			public void OnError(Exception error) { }
+			public void OnNext(DeviceChangedArgs value)
+			{
+				form.SetMute();
+			}
+		}
+
+		[DllImport("user32.dll", SetLastError = true)]
+		static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc,
+			uint idProcess, uint idThread, uint dwFlags);
+		[DllImport("user32.dll", SetLastError = true)]
+		static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+		[DllImport("User32.dll", SetLastError = true)]
+		static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
+		[DllImport("User32.dll", SetLastError = true)]
+		static extern IntPtr GetForegroundWindow();
+		[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+		static extern IntPtr FindWindow(string? lpClassName, string lpWindowName);
+		delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread,
+			uint dwmsEventTime); // delegate: function pointer
+		const uint EVENT_SYSTEM_FOREGROUND = 3;
+		const uint WINEVENT_OUTOFCONTEXT = 0;
+		//static MMDeviceEnumerator audio = new();
+		//NotificationClient? client;
+		static CoreAudioController audio = new();
+		Observer? observer;
+		IntPtr handle;
+		IntPtr eventhook;
+		GCHandle gch;
+		RegistryKey? reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+		void MuteSession(bool mute)
+		{
+			handle = FindWindow(null, "原神");
+			if (GetWindowThreadProcessId(handle, out int pid) != 0) 
+			{
+				bool unchanged = true;
+				while (unchanged)
+					audio.GetDevices(DeviceType.Playback, DeviceState.Active).SelectMany(x => x.SessionController).Where(x => x.ProcessId == pid).ToList().ForEach(x => { x.IsMuted = mute; unchanged = false; });
+			}
+		}
+		//while (unchanged)
+		//	foreach (MMDevice device in audio.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+		//		for (int i = 0; i < device.AudioSessionManager.Sessions.Count; i++)
+		//			if (device.AudioSessionManager.Sessions[i].GetProcessID == pid)
+		//			{
+		//				device.AudioSessionManager.Sessions[i].SimpleAudioVolume.Mute = mute;
+		//				unchanged = false;
+		//			}
+
+		public void SetMute(IntPtr? hwnd = null)
+		{
+			if (!啟用ToolStripMenuItem.Checked)
+				return;
+			MuteSession(handle != (hwnd ?? GetForegroundWindow()));
+		}
+
+		public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread,
+			uint dwmsEventTime)
+		{
+			SetMute(hwnd); // hwnd: handle to a window
+		}
+
+		public Form1()
+        {
+            InitializeComponent();
+			Application.ApplicationExit += ApplicationExit;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+        {
+			//client = new(this);
+			//audio.RegisterEndpointNotificationCallback(client);
+			observer = new(this);
+			audio.AudioDeviceChanged.Subscribe(observer);
+			WinEventDelegate proc = new(WinEventProc);
+			gch = GCHandle.Alloc(proc);
+			eventhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, proc, 0, 0, WINEVENT_OUTOFCONTEXT);
+			啟用ToolStripMenuItem.Checked = true;
+			if (reg?.GetValue("BGMuter") != null)
+				開機時啟動ToolStripMenuItem.Checked = true;
+		}
+
+		private void Form1_Shown(object sender, EventArgs e)
+		{
+			Hide();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (e.CloseReason == CloseReason.UserClosing)
+			{
+				Hide();
+				e.Cancel = true;
+			}
+		}
+
+		private void 啟用ToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+		{
+			MuteSession(啟用ToolStripMenuItem.Checked);
+		}
+
+		private void 開機時啟動ToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+		{
+			if (開機時啟動ToolStripMenuItem.Checked)		
+				reg?.SetValue("BGMuter", Application.ExecutablePath);
+			else
+				reg?.DeleteValue("BGMuter", false);
+		}
+
+		private void 離開ToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void Form1_Resize(object sender, EventArgs e)
+		{
+			if (WindowState == FormWindowState.Minimized)
+				Hide();
+		}
+
+		private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				Show();
+				WindowState = FormWindowState.Normal;
+				ShowInTaskbar = true;
+			}
+		}
+
+		private void ApplicationExit(object? sender, EventArgs e)
+		{
+			UnhookWinEvent(eventhook); // 解除自訂callback
+			MuteSession(false);
+			gch.Free();
+		}
+	}
+}
